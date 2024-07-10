ee8f043aed8805e268fbb502786ad1416afdc6c4
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-07-09 17:28:00 -0700

    lecture 2 Cross-Site Scripting (XSS)

---
+++ lec2WebSecurity/lec2WebSecurity/Controllers/GuestbookController.cs
@@ -0,0 +1,42 @@
+﻿using lec2WebSecurity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lec2WebSecurity.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        private static List<GuestbookEntry> _entries = new List<GuestbookEntry>();
+
+        public IActionResult Index()
+        {
+            return View(_entries);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(GuestbookEntry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                // Simulate database insertion
+                entry.Id = _entries.Count + 1;
+                entry.DatePosted = DateTime.Now;
+
+                // XSS Vulnerability: No sanitization of Message field
+                _entries.Add(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(entry);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+
+
+}
---
+++ lec2WebSecurity/lec2WebSecurity/Models/GuestbookEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace lec2WebSecurity.Models
+{
+    public class GuestbookEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime DatePosted { get; set; }
+
+    }
+}
